42f7bb69f8c8e8a7136e41d9f84f14f168ffc6a7
iscdsp <caiopacnunes@gmail.com>
2022-09-05 11:17:37 -0300

    criação das validações de data e idade com pipes e diretivas

--- LoginSenhaComponent/CRUD_WithLoginPassword/src/app/app.module.ts
+++ LoginSenhaComponent/CRUD_WithLoginPassword/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { PessoaModule } from './pessoa/pessoa.module';
 import { EnderecoModule } from './endereco/endereco.module';
 import { CidadeModule } from './cidade/cidade.module';
 import { EstadoModule } from './estado/estado.module';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
--- LoginSenhaComponent/CRUD_WithLoginPassword/src/app/pessoa/pessoa.module.ts
+++ LoginSenhaComponent/CRUD_WithLoginPassword/src/app/pessoa/pessoa.module.ts
@@ -8,13 +8,20 @@ import { ListarPessoaComponent } from './listar-pessoa/listar-pessoa.component';
 
 import { InserirPessoaComponent } from './inserir-pessoa/inserir-pessoa.component';
 import { EditarPessoaComponent } from './editar-pessoa/editar-pessoa.component';
+
+import { NumericoDirective } from '../shared/directives/numerico.directive';
+import { MinimoValidatorDirective } from '../shared/directives/minimo-validator.directive';
+
+import { NgxMaskModule, IConfig } from 'ngx-mask';
 @NgModule({
   declarations: [
     ListarPessoaComponent,
     InserirPessoaComponent,
     EditarPessoaComponent,
+    NumericoDirective,
+    MinimoValidatorDirective,
   ],
-  imports: [CommonModule, RouterModule, FormsModule],
+  imports: [CommonModule, RouterModule, FormsModule, NgxMaskModule.forRoot()],
   providers: [PessoaService],
 })
 export class PessoaModule {}
